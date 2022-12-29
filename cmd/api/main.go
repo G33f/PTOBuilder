b700b3285239aa2ad3ba9a2203fef600addf78bc
g33f <G33e33f@gmail.com>
2022-12-29 13:55:21 +0300

    add logging in project

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,30 +3,32 @@ package main
 import (
 	"PTOBuilder/config"
 	"PTOBuilder/internal/server"
+	"PTOBuilder/pkg/logging"
 	"PTOBuilder/pkg/storage"
 	"context"
-	"fmt"
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	logger := logging.GetLogger()
 	ctx := context.Background()
 	err := config.GetConfigs()
 	if err != nil {
-		fmt.Println(err)
+		logger.Fatal(err)
 	}
 	repo, err := storage.NewStorage(ctx)
 	if err != nil {
-		fmt.Println(err)
+		logger.Fatal(err)
 	}
 	err = repo.Ping(ctx)
 	if err != nil {
-		fmt.Println(err)
+		logger.Fatal(err)
 	}
 	router := httprouter.New()
 	s := server.NewServer(router)
 	err = s.Run()
 	if err != nil {
-		fmt.Println(err)
+		logger.Fatal(err)
 	}
+	logger.Info("all done right")
 }
